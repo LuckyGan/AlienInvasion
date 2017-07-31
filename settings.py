1cebb577a70c6d89f1857b3c91cbceb3510fc49e
LuckyGan <18357154046@163.com>
2017-07-31 16:07:22 +0800

    20170731 first commit

--- settings.py
+++ settings.py
@@ -7,11 +7,13 @@ class Settings():
         self.screen_width=1200
         self.screen_height=800
         self.bg_color=(230,230,230)
-        # 屏幕设置
+
+        # 飞船设置
         self.ship_speed_factor=1.5
+        self.ship_limit=3
 
         #子弹设置
-        self.bullet_speed_factor=1
+        self.bullet_speed_factor=3
         self.bullet_width=3
         self.bullet_height=15
         self.bullet_color=(60,60,60)
